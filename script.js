92682a0f08a23bb0426bd1061fa8f31e5bcc9fd8
MuhammadAhmed321 <muhammadahmedrafiq58@gmail.com>
2023-10-26 18:16:43 +0500

    First Commit

---
+++ script.js
@@ -0,0 +1,73 @@
+let key = "11da654ab4f94fe1afe962881878f296";
+let cardData = document.querySelector(".cardData");
+let SearchBtn = document.getElementById("searchBtn");
+let inputData = document.getElementById("inputData");
+let searchType = document.getElementById("type");
+
+const getData = async (input) => {
+    let res = await fetch(`https://newsapi.org/v2/everything?q=${input}&apiKey=${key}`);
+    let jsonData = await res.json();
+    console.log(jsonData.articles);
+
+    searchType.innerText = "search : " + input;
+    inputData.value = ""
+    cardData.innerHTML = "";
+    jsonData.articles.forEach(function (article) {
+        console.log(article);
+
+        let divs = document.createElement("div");
+        divs.classList.add("card");
+        cardData.appendChild(divs);
+
+        divs.innerHTML = `
+    <img src="${article.urlToImage}" alt="">
+    <h3>${article.title}</h3>
+    <p>${article.description}</p>
+    
+    `
+
+        // divs.addEventListener("click", function() {
+        //     window.open(article.url);
+        // })
+
+    })
+
+
+
+
+}
+
+window.addEventListener("load", function () {
+    getData("Pakistan")
+})
+
+// window.
+SearchBtn.addEventListener("click", function () {
+    let inputText = inputData.value;
+    getData(inputText);
+
+})
+
+function navClick(navName) {
+    if (navName == "politics") {
+        document.getElementById("politics").style.color = "red"
+        document.getElementById("sports").style.color = "white"
+        document.getElementById("technology").style.color = "white"
+    }
+
+    if (navName == "sports") {
+        document.getElementById("politics").style.color = "white"
+        document.getElementById("sports").style.color = "red"
+        document.getElementById("technology").style.color = "white"
+    }
+
+    if (navName == "technology") {
+        document.getElementById("politics").style.color = "white"
+        document.getElementById("sports").style.color = "white"
+        document.getElementById("technology").style.color = "red"
+    }
+    getData(navName)
+}
+
+
+
